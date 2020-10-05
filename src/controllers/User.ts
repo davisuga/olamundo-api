31e8c23228224aa031fcb934530471952db1dd10
Pedro <cl3t0.sh@gmail.com>
2020-10-05 01:13:13 -0300

    feat(src/controllers): created a basic crud to all 3 tables

---
+++ src/controllers/User.ts
@@ -0,0 +1,64 @@
+import { Router } from "express";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+const find_all = async (req: any, res: any) => {
+    const result = await prisma.user.findMany();
+    res.json(result);
+}
+const find_one = async (req: any, res: any) => {
+    try {
+        const result = await prisma.user.findOne({
+            where: { id: parseInt(req.params.id) }
+        });
+        res.json(result);
+    } catch (error) {
+        res.status(400);
+        res.json({ error });
+    }
+}
+const create_one = async (req: any, res: any) => {
+    try {
+        const result = await prisma.user.create({
+            data: req.body
+        });
+        res.json(result);    
+    } catch (error) {
+        res.status(400);
+        res.json({ error });
+    }
+    
+}
+const delete_one = async (req: any, res: any) => {
+    try {
+        const result = await prisma.user.delete({
+            where: { id: parseInt(req.params.id) }
+        });
+        res.json(result);
+    } catch (error) {
+        res.status(400);
+        res.json({ error });
+    }
+}
+const update_one = async (req: any, res: any) => {
+    try {
+        const result = await prisma.user.update({
+            where: { id: parseInt(req.params.id) },
+            data: req.body
+        });
+        res.json(result);    
+    } catch (error) {
+        res.status(400);
+        res.json({ error });
+    }
+}
+
+const routes = Router();
+routes.get('/', find_all);
+routes.get('/:id', find_one);
+routes.post('/', create_one);
+routes.delete('/:id', delete_one);
+routes.put('/:id', update_one);
+
+export default routes;
--- src/index.ts
+++ src/index.ts
@@ -1,46 +1,13 @@
-import { PrismaClient } from "@prisma/client";
 import express from "express";
-import * as bodyParser from "body-parser";
+import routes from "./routes";
 
-const prisma = new PrismaClient();
-const app = express();
+const PORT: number = 3000;
 
-app.use(bodyParser.json());
+const app: express.Application = express();
 
-app.post("/user", async (req, res) => {
-  const { name, email, password } = req.body;
-  const result = await prisma.user.create({
-    data: {
-      name,
-      email,
-      password,
-    },
-    select: {
-      name,
-    },
-  });
-  res.json(result);
-});
+app.use(express.json());
+app.use(routes);
 
-app.get("/user", async (req, res) => {
-  const { email } = req.body;
-  const result = await prisma.user.findOne({
-    where: { email },
-  });
-  res.json(result);
-});
-
-app.get("/exercise", async (req, res) => {
-  const { email } = req.body;
-  const result = await prisma.user.findOne({
-    where: { email },
-  });
-  res.json(result);
-});
-app.listen(3000, () => {
-  console.log(`Example app listening at http://localhost:${3000}`);
-});
-
-app.get("/", async (req, res) => {
-  res.send("<h1>Ooops</h1>");
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
 });
